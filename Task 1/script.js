f9a8bdb301ce5b26810913a6dd8925bee3b3c1f1
mkuzminas <marius.kuzminas@gmail.com>
2022-02-23 21:16:27 +0200

    Completed Task 1

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,36 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const form = document.querySelector('form')
+const output = document.querySelector('#output')
+
+
+form.addEventListener("submit", calculate);
+
+function calculate(e) {
+    e.preventDefault();
+    const weigthKg = document.querySelector("#search").value;
+    console.log(weigthKg);
+    const weigthLb = weigthKg * 2.2046;
+    const weigthG = weigthKg / 0.0010000;
+    const weigthOz = weigthKg * 35.274;
+    let list = document.createElement('ul');
+    let liLb = document.createElement('li');
+    liLb.innerText = `Weigth in lb is: ${weigthLb}`
+    let liG = document.createElement('li');
+    liG.innerText = `Weigth in g is: ${weigthG}`
+    let liOz = document.createElement('li');
+    liOz.innerText = `Weigth in oz is: ${weigthOz}`
+
+
+    output.innerText = "";
+    output.appendChild(list);
+    list.appendChild(liLb);
+    list.appendChild(liG);
+    list.appendChild(liOz);
+
+
+    output.style.cssText = "color: blue; font-weight: bold; text-align: center; font-size: 3em"
+    form.reset();
+
+}
